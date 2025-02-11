c89d3902664276c5888d0d9bd251d3d735235b84
Azlan Shah <azlan@kestrl.io>
2025-02-12 01:59:27 +0800

    Update pledge checkbox style

--- routes/pledge.tsx
+++ routes/pledge.tsx
@@ -162,17 +162,17 @@ export default function PledgePage({ data }: PageProps<PledgePageData>) {
               />
             </div>
 
-            <div class="flex items-center ps-4 border border-gray-200 rounded-sm dark:border-gray-700">
+            <div class="flex items-center ps-4 border border-gray-200 rounded-md">
               <input
                 id="ikrar"
                 type="checkbox"
                 value=""
                 name="ikrar"
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-200 rounded-md focus:ring-blue-500 focus:ring-2"
               />
               <label
                 for="ikrar"
-                class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                class="w-full py-4 ms-4 text-sm font-medium text-gray-700"
               >
                 Dengan lafaz Bismillah, saya berikrar untuk menyumbangkan jumlah
                 yang dinyatakan diatas kepada Rumah Perlindungan Kanak-kanak TBQ
