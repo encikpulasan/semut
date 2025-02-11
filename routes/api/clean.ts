b6de789df7ad82d1205b25fa3f96c06f5f316b7a
Azlan Shah <azlan@kestrl.io>
2025-02-12 00:12:42 +0800

    Change to static clean key

--- routes/api/clean.ts
+++ routes/api/clean.ts
@@ -3,13 +3,13 @@ import { Handlers } from "$fresh/server.ts";
 import { cleanAllData } from "../../utils/clean.ts";
 
 export const handler: Handlers = {
-    async POST(req) {
-        const adminKey = req.headers.get("admin-key");
-        if (adminKey !== Deno.env.get("ADMIN_KEY")) {
-            return new Response("Unauthorized", { status: 401 });
-        }
+  async POST(req) {
+    const adminKey = req.headers.get("admin-key");
+    if (adminKey !== "our-secret-key") {
+      return new Response("Unauthorized", { status: 401 });
+    }
 
-        await cleanAllData();
-        return new Response("Data cleaned successfully");
-    },
+    await cleanAllData();
+    return new Response("Data cleaned successfully");
+  },
 };
